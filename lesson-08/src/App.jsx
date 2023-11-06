03f3c554d5181d25089cd38796e61dc643990e4b
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-06 14:33:23 +0100

    update

--- lesson-08/src/App.jsx
+++ lesson-08/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import NewPost from "./NewPost";
 import PostPage from "./PostPage";
@@ -10,44 +10,18 @@ import HomeLayout from "./HomeLayout";
 import EditPost from "./EditPost";
 import { DataProvider } from "./context/DataContext";
 
-
 const App = () => {
-
-
-
-
   return (
     <DataProvider>
       <Routes>
-        <Route
-          path="/"
-          element={<HomeLayout />}
-        >
-          <Route index element={<Home />}
-          />
+        <Route path="/" element={<HomeLayout />}>
+          <Route index element={<Home />} />
           <Route path="/post">
-            <Route
-              index
-              element={
-                <NewPost
+            <Route index element={<NewPost />} />
 
-                />
-              }
-            />
-
-            <Route
-              path=":id"
-              element={<PostPage />}
-            />
+            <Route path=":id" element={<PostPage />} />
           </Route>
-          <Route
-            path="/edit/:id"
-            element={
-              <EditPost
-
-              />
-            }
-          />
+          <Route path="/edit/:id" element={<EditPost />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Missing />} />
         </Route>
