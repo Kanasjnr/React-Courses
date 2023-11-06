1370e728e0a0c09d39892c25df01b97fdd9c9ca2
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-06 09:55:05 +0100

    update

--- lesson-08/src/context/DataContext.jsx
+++ lesson-08/src/context/DataContext.jsx
@@ -1,3 +1,18 @@
 import { createContext, useEffect, useState } from "react";
 
 const DataContext = createContext({})
+
+export const DataProvider = ({ children }) => {
+    return (
+        <DataContext.Provider value={{
+
+        }}>
+            {children}
+        </DataContext.Provider>
+    )
+}
+
+
+
+
+export default DataContext
