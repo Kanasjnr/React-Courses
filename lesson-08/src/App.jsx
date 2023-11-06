2ecf8118efbe824c785b30112b966a331e6ce2c3
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-06 10:28:37 +0100

    updare

--- lesson-08/src/App.jsx
+++ lesson-08/src/App.jsx
@@ -9,8 +9,6 @@ import About from "./About";
 import HomeLayout from "./HomeLayout";
 import api from "./api/posts";
 import EditPost from "./EditPost";
-import UseWindowSize from "./hooks/useWindowSize";
-import useAxiosFetch from "./hooks/useAxiosFetch";
 import { DataProvider } from "./context/DataContext";
 
 
