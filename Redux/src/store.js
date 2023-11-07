094e72121a81e125a7e3546141aa1d0f6edd9335
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-07 10:20:03 +0100

    update

--- Redux/src/store.js
+++ Redux/src/store.js
@@ -3,7 +3,28 @@ import api from "./api/posts";
 
 export default createStore({
   posts: [],
-  setPosts: action((state) => {}),
+  setPosts: action((state, payload) => {
+    state.posts = payload;
+  }),
+  postTitle: " ",
+  setPostTitle: action((state, payload) => {
+    state.postTitle = payload;
+  }),
+  postBody: " ",
+  setPostBody: action((state, payload) => {
+    state.postBody = payload;
+  }),
+  editTitle:" ",
+  setEditTitle: action((state, payload) => {
+    state.editTitle = payload;
+  }),
+  editBody:" ",
+  setEditBody: action((state, payload) => {
+    state.editBody = payload;
+  }),
+
+  search:"",
+  setSearch: action((state, payload) => {
+    state.search = payload;
+  })
 });
-
-
