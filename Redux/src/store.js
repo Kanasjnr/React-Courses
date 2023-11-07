18699c5a80b492b4a4083a8b22d678c65663d160
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-07 10:06:25 +0100

    update

--- Redux/src/store.js
+++ Redux/src/store.js
@@ -1,9 +1,11 @@
 import { createStore, action, thunk, computed } from "easy-peasy";
 import api from "./api/posts";
-import posts from "./api/posts";
 
-export default createStore(
+
+export default createStore({
     posts: [],
-    setPosts: action()
+    setPosts: action(() => {})
+}
+   
 
 );
