9b2d8e4d6477c1d1fea3f6605be02491c7aed25a
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-07 11:36:05 +0100

    update

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -6,27 +6,26 @@ import PostPage from "./PostPage";
 import Missing from "./Missing";
 import About from "./About";
 import HomeLayout from "./HomeLayout";
-
 import EditPost from "./EditPost";
-import { DataProvider } from "./context/DataContext";
+
 
 const App = () => {
   return (
-    <DataProvider>
-      <Routes>
-        <Route path="/" element={<HomeLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/post">
-            <Route index element={<NewPost />} />
 
-            <Route path=":id" element={<PostPage />} />
-          </Route>
-          <Route path="/edit/:id" element={<EditPost />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Missing />} />
+    <Routes>
+      <Route path="/" element={<HomeLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/post">
+          <Route index element={<NewPost />} />
+
+          <Route path=":id" element={<PostPage />} />
         </Route>
-      </Routes>
-    </DataProvider>
+        <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+
   );
 };
 
