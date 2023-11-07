f243e2df02be6b21aafe83161ffcf0c23865fe62
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-07 12:30:11 +0100

    a

--- Redux/src/PostPage.jsx
+++ Redux/src/PostPage.jsx
@@ -1,24 +1,17 @@
-
 import { Link, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { useStoreState,useStoreActions } from "easy-peasy";
-
+import { useStoreState, useStoreActions } from "easy-peasy";
 
 const PostPage = () => {
-
   const navigate = useNavigate();
   const { id } = useParams();
-  const deletePost = useStoreActions((actions) => actions.posts)
-  const getPostById = useStoreState((state) => state.getPostById)
-  const post = getPostById(id)
+  const deletePost = useStoreActions((actions) => actions.deletePost);
+  const getPostById = useStoreState((state) => state.getPostById);
+  const post = getPostById(id);
 
   const handleDelete = async (id) => {
-   
-      navigate("/");
-   
+    navigate("/");
   };
-
-
 
   return (
     <main className="PostPage">
