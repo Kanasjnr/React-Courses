af35440c0584745961c53abf4917efcfb8d35519
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-07 09:30:54 +0100

    update

---
+++ Redux/src/Header.jsx
@@ -0,0 +1,20 @@
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
+import UseWindowSize from "./hooks/useWindowSize";
+
+const Header = ({ title }) => {
+  const { width } = UseWindowSize();
+  return (
+    <header className="Header">
+      <h1>{title}</h1>
+      {width < 768 ? (
+        <FaMobileAlt />
+      ) : width < 992 ? (
+        <FaTabletAlt />
+      ) : (
+        <FaLaptop />
+      )}
+    </header>
+  );
+};
+
+export default Header;
