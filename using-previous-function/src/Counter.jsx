4e2970f94c584c62d2a414a3cd86426dd5b9a385
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-08 10:55:56 +0100

    upadte

--- using-previous-function/src/Counter.jsx
+++ using-previous-function/src/Counter.jsx
@@ -5,15 +5,19 @@ import React from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState([]);
+
   const plusButton = () => {
     // setCount(count + 1)
-    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1); //using previous
     setCount((prev) => prev + 1);
   };
   const minusButton = () => {
     setCount((prev) => prev - 1);
     setCount((prev) => prev - 1);
   };
+
+  const updateArray = () => {};
 
   const resetButton = () => setCount(0);
   return (
