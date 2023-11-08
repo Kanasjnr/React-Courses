d6f609e2cffe5af40cf415ea692279174171cd40
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-08 12:06:57 +0100

    a

--- use-callback/src/App.jsx
+++ use-callback/src/App.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 const App = () => {
   const [userInput,setUserInput] = useState('')
   const [num1] = useState(4)
+  const [num2] = useState(5)
   return (
     <div>App</div>
   )
