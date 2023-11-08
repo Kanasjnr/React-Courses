5221f2434e37b8393014c5d10c1537e87238fc0f
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-08 10:18:12 +0100

    update

--- using-previous-function/src/Counter.jsx
+++ using-previous-function/src/Counter.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
+import "./index.css";
 
 import React from "react";
 
 const Counter = () => {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
+  const plusButton = () => setCount(count + 1);
+  const minusButton = () => setCount(count - 1);
+  const resetButton = () => setCount(0);
   return (
-    <section>
+    <section >
       <h1>{count}</h1>
-      <div>
-        <button>+</button>
-        <button>-</button>
+
+      <div >
+        <button onClick={plusButton}> +</button>
+        <button onClick={minusButton}> -</button>
+       
+      </div>
+      <div className="rest">
+      <button onClick={resetButton}> Reset </button>
       </div>
     </section>
   );
