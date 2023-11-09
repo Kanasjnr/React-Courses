5886aa58df47a90eb7a21d367dfcf7651314ce63
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-09 10:45:05 +0100

    a

--- react-use-memo/src/App.jsx
+++ react-use-memo/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState, useEffect,useCallback,useMemo } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 
-const getArray =() =>{
-  for(let i = 0; i < 1000000000; i++){
-
-
-  }
-  return ['Muhammed', "Fine Boy"]
-}
+const getArray = () => {
+  for (let i = 0; i < 1000000000; i++) {}
+  return ["Muhammed", "Fine Boy"];
+};
 
 const App = () => {
   const [userInput, setUserInput] = useState("");
-  const [randomInput, setRandomInput] = useState('')
+  const [randomInput, setRandomInput] = useState("");
 
   const fib = useCallback((n) => {
-    return n <= 1 ? n: fib(n-1) + fib(n-2)
-  },[])
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+  }, []);
 
-  const fibNumber = useMemo(() => fib(userInput), [userInput,fib])
+  const fibNumber = useMemo(() => fib(userInput), [userInput, fib]);
 
-  const myArray = useMemo(()=> getArray(),[])
- 
-  return(
+  const myArray = useMemo(() => getArray(), []);
+
+  return (
     <main className="App">
-      <input type="text" placeholder="input" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
+      <input
+        type="text"
+        placeholder="input"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
       <h1>output:{userInput || ".."}</h1>
     </main>
-  )
-
-
-}
+  );
+};
 
 export default App;
