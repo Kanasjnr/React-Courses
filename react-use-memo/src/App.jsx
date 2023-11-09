a8dd23beca01ee66a58388ebd05df5256e68e6f4
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-09 10:43:51 +0100

    a

--- react-use-memo/src/App.jsx
+++ react-use-memo/src/App.jsx
@@ -17,6 +17,8 @@ const App = () => {
   },[])
 
   const fibNumber = useMemo(() => fib(userInput), [userInput,fib])
+
+  const myArray = useMemo(()=> getArray(),[])
  
   return(
     <main className="App">
