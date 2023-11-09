ddf9af4b5a0400154a77a05afe5b43843901ca78
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-09 15:30:50 +0100

    a

--- react-useReduce/src/App.jsx
+++ react-useReduce/src/App.jsx
@@ -1,22 +1,26 @@
 import { useReducer, useState } from "react";
 
 const reducer = (state, action) => {
-  switch(action.type) {
-    case'increment':
-    return{...state, count: state.count + 1}
-    case'decrement':
-    return{...state, count: state.count - 1}
-    case 'newUserInput':
-      return {userInput: action.payload}
-      case'tgColor':
-      return {...state, color: !state.color}
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    case "newUserInput":
+      return { userInput: action.payload };
+    case "tgColor":
+      return { ...state, color: !state.color };
     default:
-      throw new Error()
+      throw new Error();
   }
-}
+};
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer,{count: 0, userInput:'', color:false})
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    userInput: "",
+    color: false,
+  });
   // const [userInput, setUserInput] = useState("");
   // // const [count, setCount] = useState(0);
   // const [color, setColor] = useState(false);
@@ -24,30 +28,29 @@ const App = () => {
   return (
     <main className="App" style={{ color: color ? "#fff" : "#fff952" }}>
       <input
-     
         type="text"
         value={userInput}
-        onChange={(e) => dispatch({type:'newUserInput', payload: e.target.value})}
-     
+        onChange={(e) =>
+          dispatch({ type: "newUserInput", payload: e.target.value })
+        }
       />
 
       <br />
       <br />
       <p> {state.count}</p>
-{/* use state */}
+      {/* use state */}
       {/* <section>
         <button onClick={() => setCount((prev) => prev - 1)}>-</button>
         <button onClick={() => setCount((prev) => prev + 1)}>+</button>
         <button onClick={() => setColor((prev) => !prev)}>Color</button>
       </section> */}
 
-<section>
-        <button onClick={(() => dispatch({type: 'decrement'}))}>-</button>
-        <button onClick={(() => dispatch({type: 'increment'}))}>+</button>
-       
-        <button onClick={(() => dispatch({type: 'tgColor'}))}>Color</button>
-      </section>
+      <section>
+        <button onClick={(() => dispatch({ type: "decrement" }))}>-</button>
+        <button onClick={(() => dispatch({ type: "increment" }))}>+</button>
 
+        <button onClick={(() => dispatch({ type: "tgColor" }))}>Color</button>
+      </section>
 
       <br />
       <br />
