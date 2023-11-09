2aca86788bba0ee5daab08771263dbbd126aee0c
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-09 15:04:24 +0100

    a

--- react-useReduce/src/App.jsx
+++ react-useReduce/src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
       <p> {count}</p>
 
       <section>
-        <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+        <button onClick={() =>dispatch({type:'decrement'})}>-</button>
         <button onClick={() => setCount((prev) => prev + 1)}>+</button>
         <button onClick={() => setColor((prev) => !prev)}>Color</button>
       </section>
