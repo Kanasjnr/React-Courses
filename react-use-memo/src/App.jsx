da1d310d8e6c0871dff129aa2f44770c3445f248
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-09 10:13:19 +0100

    a

--- react-use-memo/src/App.jsx
+++ react-use-memo/src/App.jsx
@@ -9,23 +9,7 @@ const getArray =() =>{
 
 const App = () => {
   const [userInput, setUserInput] = useState("");
-  const [result, setResult] = useState(0)
-  const [num1] = useState(4);
-  const [num2] = useState(5);
-
-
-  const sum = useCallback(()  => num1 + num2, [num1,num2])
-
-  const buildArray = useCallback(() => [num1,num2], [num1,num2])
-
-  
-
-  useEffect(() =>{
-    console.log(`New sum.value: ${buildArray()}`);
-    setResult(buildArray())
-
-  },[buildArray])
-
+ 
   return(
     <main className="App">
       <input type="text" placeholder="input" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
