e900b983474b13e21d2afefdfc9e7ba6849b0371
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-21 09:01:55 +0100

    update

---
+++ react-async-await-request/src/components/Posts.jsx
@@ -0,0 +1,16 @@
+import axios from "../apis/jsonPH";
+import { useEffect } from "react";
+import useAxiosFunction from "../hooks/useAxiosFunction"
+
+
+import React from 'react'
+
+const Posts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Posts
