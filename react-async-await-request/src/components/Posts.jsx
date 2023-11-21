7af46f4a69479bac3c53ce8e908ae4793884c042
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-21 09:23:26 +0100

    e

--- react-async-await-request/src/components/Posts.jsx
+++ react-async-await-request/src/components/Posts.jsx
@@ -25,11 +25,10 @@ const Posts = () => {
       url: "/posts",
       requestConfig: {
         data: {
-          userId: 1,
-          id: 1,
-          title:
-            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-          body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+          userId: 10,
+
+          title: "Supreme",
+          body: "Boda Oni Chicken...",
         },
       },
     });
