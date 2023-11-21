0c70297ef387633b69d0d53e61a78176bc574afb
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-21 09:22:01 +0100

    update

--- react-async-await-request/src/components/Posts.jsx
+++ react-async-await-request/src/components/Posts.jsx
@@ -13,18 +13,27 @@ const Posts = () => {
     });
   };
 
-  useEffect (()=>{
+  useEffect(() => {
     //TO GET THE DATA FROM THE ENDPOINT
     getData();
-  },[])
+  }, []);
 
-  const handleSubmit = () =>{
+  const handleSubmit = () => {
     axiosFetch({
-        axiosInstance: axios,
-        method: "POST",
-        url: "/posts",
-      });
-  }
+      axiosInstance: axios,
+      method: "POST",
+      url: "/posts",
+      requestConfig: {
+        data: {
+          userId: 1,
+          id: 1,
+          title:
+            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        },
+      },
+    });
+  };
 
   return <div></div>;
 };
